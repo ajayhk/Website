80ceb0ed66ff2aff33778f13a9109bce538ec59c
Ajay <ajay.harikumar@gmail.com>
2015-08-14 18:20:20 -0700

    Minor mods to links

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
  
 func main() {
-    http.Handle("/", http.FileServer(http.Dir("~/workspace/goWS/src/github.com/Website/static/")))
+    http.Handle("/", http.FileServer(http.Dir("/home/anon/workspace/goWS/src/github.com/Website/static/")))
     err := http.ListenAndServe(":80", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
